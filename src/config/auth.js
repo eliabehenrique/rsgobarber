3b719e11519c42e1e0b4c7145cb2eb011303ac2f
= <=>
2019-06-30 07:59:21 -0300

    migration user, user and session ctrl, auth pt.1, config database

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0a36232a8c46fec99602d9ebd70daa7a',
+  expiresIn: '2d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import SessionsController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
 
 export default routes;
