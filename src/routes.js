b602c8a51505dbb906cdd00071f60c26d71061c4
= <=>
2019-07-03 01:01:08 -0300

    new api - list schedule by provider

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionsController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import FileController from './app/controllers/FileController';
 
 const routes = new Router();
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
